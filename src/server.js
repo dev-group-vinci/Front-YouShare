7775e00fcb590040c3505c28ba70085b1bf43f3b
Eliott Rasumny <eliott.rasumny@student.vinci.be>
2022-12-08 16:33:32 +0100

    test pipeline

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/<name-of-app>"));
+app.use(express.static(__dirname + "/dist/front-youshare-dev"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/<name-of-app>/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/front-youshare-dev/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
