5201834a0dcf9796adfad333aa90964c2230a6e8
Zoé Levaux <zoe.levaux@student.vinci.be>
2022-12-09 12:12:01 +0100

    profile page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { DataService } from './services/data.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from '../components/login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from '../components/home/home.component';
-import { RegisterComponent } from '../components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
 import { NgToastModule } from 'ng-angular-popup';
 import { YouTubePlayerModule } from '@angular/youtube-player';
 import { NgxSpinnerModule } from "ngx-spinner";
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
