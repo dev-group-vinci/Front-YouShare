d9f0cbfe6e05a4ed31143f7a1b40e9cd33b35fd3
Eliott Rasumny <eliott.rasumny@student.vinci.be>
2022-12-10 19:20:25 +0100

    get picture from back not working

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import {environment} from "../../environments/environment.dev";
+import { Picture } from '../models/picture.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUrl = environment.apiUrl;
+  constructor(
+    private _http: HttpClient,
+    private router: Router,
+    ) { }
+
+  getPicture(id: number) {
+    let temp = this._http.get<Picture>(`${this.apiUrl}users/1/picture`);
+    console.log(temp);
+    
+    return temp;
+  }
+}
