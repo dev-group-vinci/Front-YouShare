f0224711487375f5c195ead0f689791711fabcbb
Zoé Levaux <zoe.levaux@student.vinci.be>
2022-12-11 13:02:25 +0100

    friend requests design

--- src/app/components/friendlist/friendlist.component.ts
+++ src/app/components/friendlist/friendlist.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { DataService } from 'src/app/services/data.service';
 import { Router } from '@angular/router';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {User} from "../../models/user.model";
 
 @Component({
   selector: 'app-friendlist',
@@ -10,14 +12,22 @@ import { Router } from '@angular/router';
 })
 export class FriendlistComponent {
 
+  friendsForm!: FormGroup;
+  user$: User;
+
   constructor(
+    private fb: FormBuilder,
     private router: Router,
-    private dataService: DataService, 
+    private data: DataService,
     private auth: AuthService,
   ) {}
 
   ngOnInit(): void {
-
+    this.data.getUserLoggedIn().subscribe({
+      next: (res) => {
+        this.user$ = new User(res);
+      }
+    })
   }
 
   logout(){
